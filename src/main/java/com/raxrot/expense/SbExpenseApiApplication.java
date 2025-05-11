ebaf58d5061824814a0eabcf897480ccbb7f06a5
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-05-11 19:57:34 +0100

    init

---
+++ src/main/java/com/raxrot/expense/SbExpenseApiApplication.java
@@ -0,0 +1,13 @@
+package com.raxrot.expense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbExpenseApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbExpenseApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/raxrot/expense/SbExpenseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raxrot.expense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbExpenseApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
