393136b85c77a73b012552a8e4a1946a02d7b654
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-05-12 21:20:44 +0100

    add Cross origin to allow to work with react

--- src/main/java/com/raxrot/expense/controller/ExpenseController.java
+++ src/main/java/com/raxrot/expense/controller/ExpenseController.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @Slf4j
 @RestController
 @RequestMapping("/api/v1")
